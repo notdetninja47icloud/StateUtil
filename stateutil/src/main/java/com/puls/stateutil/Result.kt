6b1cb691a829f21ebc3476c55f85a208ed6ea0d5
impossible1770 <alex.g7766>
2022-03-21 12:09:40 +0200

    Added new state IDLE, update kotlin versions

--- stateutil/src/main/java/com/puls/stateutil/Result.kt
+++ stateutil/src/main/java/com/puls/stateutil/Result.kt
@@ -5,4 +5,5 @@ sealed class Result<out T> {
     data class Failure(val exception: Throwable) : Result<Nothing>()
     data class Error(val code: Int, val message: String) : Result<Nothing>()
     data class Loading(val state: Boolean) : Result<Nothing>()
+    class IDLE : Result<Nothing>()
 }
